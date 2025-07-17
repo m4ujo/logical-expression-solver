fab368bb5c01bb35888fbeb5236b36175448caaf
Mauricio Castillo <mauriciocast11@outlook.com>
2025-07-17 15:29:18 -0500

    🔨 Add truthtable functions

--- TruthTable.cs
+++ TruthTable.cs
@@ -28,5 +28,67 @@ namespace karnaugh_map_solver
 
       Vars = Array.CreateInstance(typeof(bool), comb, 5);
     }
+
+    public bool Operations(bool a, bool b, bool c, bool d)
+    {
+      bool temp = false;
+      try
+      {
+        if (nVars == 2) temp = Eval.Execute<bool>(op, new { a, b });
+        if (nVars == 3) temp = Eval.Execute<bool>(op, new { a, b, c });
+        if (nVars == 4) temp = Eval.Execute<bool>(op, new { a, b, c, d });
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+        verif = true;
+      }
+      return temp;
+    }
+
+    public void InsertValues()
+    {
+      int temp;
+      int div = 1;
+      bool value = true, a, b, c, d, F;
+
+      for (int indexV = 0; indexV < nVars; indexV++)
+      {
+        temp = comb / (2 * div);
+        for (int indexXYZW = 0; indexXYZW < comb; indexXYZW++)
+        {
+          if (indexXYZW % temp == 0)
+            value = !value;
+          Vars.SetValue(value, indexXYZW, indexV);
+          a = Convert.ToBoolean(Vars.GetValue(indexXYZW, 0));
+          b = Convert.ToBoolean(Vars.GetValue(indexXYZW, 1));
+          c = Convert.ToBoolean(Vars.GetValue(indexXYZW, 2));
+          d = Convert.ToBoolean(Vars.GetValue(indexXYZW, 3));
+          F = Operations(a, b, c, d);
+          Vars.SetValue(F, indexXYZW, nVars);
+        }
+        div *= 2;
+      }
+    }
+
+    public void CreateTable()
+    {
+      for (int i = 0; i < comb; i++)
+        for (int j = 0; j < nVars + 1; j++)
+          Table[j, i].Value = ((Convert.ToBoolean(Vars.GetValue(i, j)) == true) ? 1 : 0);
+    }
+
+    public void SelectMinTerms()
+    {
+      for (int i = 0; i < comb; i++)
+        if ((Convert.ToBoolean(Vars.GetValue(i, nVars)) == true))
+          Table[nVars, i].Style.BackColor = c;
+    }
+
+    public void ClearMinTerms()
+    {
+      for (int i = 0; i < comb; i++)
+        Table[nVars, i].Style.BackColor = Color.FromArgb(61, 61, 61);
+    }
   }
 }
