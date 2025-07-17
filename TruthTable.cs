846b5d7d2d0bbc4827b0dd72c9901476c6739889
Mauricio Castillo <mauriciocast11@outlook.com>
2025-07-17 15:24:08 -0500

    ✨ Add truthtable class

---
+++ TruthTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using Z.Expressions;
+
+namespace karnaugh_map_solver
+{
+  public class TruthTable
+  {
+    public int nVars { get; set; }
+    public int comb { get; set; }
+    public Array Vars { get; set; }
+    public bool verif { get; set; }
+    public string op { get; set; }
+    public DataGridView Table { get; set; }
+    public Color c { get; set; }
+
+    public TruthTable(int nVars, string op)
+    {
+      this.nVars = nVars;
+      comb = Convert.ToInt32(Math.Pow(2, nVars));
+
+      op = op.ToLower();
+      op = op.Replace(" ", ""); op = op.Replace("^", "&&"); op = op.Replace("v", "||"); op = op.Replace("~", "!");
+
+      this.op = op;
+
+      Vars = Array.CreateInstance(typeof(bool), comb, 5);
+    }
+  }
+}
