79d01dc9e2b9fc772542cf1b2a5fbd488d7fc635
Mauricio Castillo <mauriciocast11@outlook.com>
2025-07-17 16:24:56 -0500

    ✨ Add coverage class

---
+++ Coverage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace karnaugh_map_solver 
+{
+  public class Coverage : HashSet<Group>
+  {
+    public Coverage(IEnumerable<Group> collection) : base(collection)
+    {
+
+    }
+
+    public Coverage() : base()
+    {
+
+    }
+
+    public long? Cost { get; set; }
+
+    public override bool Equals(object obj)
+    {
+      if (obj.GetType() != GetType())
+        return false;
+
+      var other = (Coverage)obj;
+      return this.SequenceEqual(other);
+    }
+
+    public override int GetHashCode()
+    {
+      if (Count > 0)
+        return this.First().GetHashCode();
+      else
+        return GetHashCode();
+    }
+  }
+}
