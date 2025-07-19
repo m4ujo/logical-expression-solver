741b7c8da836d760e9cc3a34af581c0ed0d81ff7
Mauricio Castillo <mauriciocast11@outlook.com>
2025-07-19 07:07:08 -0500

    ✨ Add utils class

---
+++ Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace karnaugh_map_solver
+{
+  public static class Utils
+  {
+
+  }
+}
