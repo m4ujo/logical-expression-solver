0aa1862f4c3bd6e16c2e7306881fd047044f5a74
Mauricio Castillo <mauriciocast11@outlook.com>
2025-07-21 12:44:04 -0500

    🔨 Update variables name

--- Map.cs
+++ Map.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Drawing;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace karnaugh_map_solver
 {
@@ -18,11 +16,11 @@ namespace karnaugh_map_solver
     public Map(TruthTable truthT)
     {
       this.truthT = truthT;
-      if (this.truthT.nVars % 2 == 0)
-        rows = columns = Convert.ToInt32(Math.Sqrt(Math.Pow(2, this.truthT.nVars)));
+      if (this.truthT.nvars % 2 == 0)
+        rows = columns = Convert.ToInt32(Math.Sqrt(Math.Pow(2, this.truthT.nvars)));
       else
       {
-        columns = Convert.ToInt32(Math.Sqrt(Math.Pow(2, this.truthT.nVars + 1)));
+        columns = Convert.ToInt32(Math.Sqrt(Math.Pow(2, this.truthT.nvars + 1)));
         rows = columns / 2;
       }
 
@@ -31,9 +29,9 @@ namespace karnaugh_map_solver
     public Array ChangeValues(Array f, int im, int iM)
     {
       Array m = f;
-      var temp = new Object[truthT.nVars + 1];
+      var temp = new Object[truthT.nvars + 1];
 
-      for (int j = 0; j < truthT.nVars + 1; j++)
+      for (int j = 0; j < truthT.nvars + 1; j++)
       {
         temp[j] = m.GetValue(im, j);
         m.SetValue(m.GetValue(iM, j), im, j);
@@ -45,22 +43,22 @@ namespace karnaugh_map_solver
 
     public void FillMap()
     {
-      if (truthT.nVars == 3 || truthT.nVars == 4)
+      if (truthT.nvars == 3 || truthT.nvars == 4)
       {
         for (int i = 0, j = 0; i < truthT.comb && j < truthT.comb; i++, j++)
           if (i == 2 || i == 6 || i == 10 || i == 14)
-            truthT.Vars = ChangeValues(truthT.Vars, i, i + 1);
+            truthT.tableValues = ChangeValues(truthT.tableValues, i, i + 1);
 
-        if (truthT.nVars == 4)
+        if (truthT.nvars == 4)
           for (int j = 0; j < truthT.comb; j++)
             if (j == 8 || j == 9 || j == 10 || j == 11)
-              truthT.Vars = ChangeValues(truthT.Vars, j, j + 4);
+              truthT.tableValues = ChangeValues(truthT.tableValues, j, j + 4);
       }
 
       int index = 0;
       for (int i = 0; i < rows; i++)
         for (int j = 0; j < columns; j++, index++)
-          matrix.SetValue(truthT.Vars.GetValue(index, truthT.nVars), i, j);
+          matrix.SetValue(truthT.tableValues.GetValue(index, truthT.nvars), i, j);
     }
 
     public void CreateMap()
--- TruthTable.cs
+++ TruthTable.cs
@@ -8,35 +8,31 @@ namespace karnaugh_map_solver
 {
   public class TruthTable
   {
-    public int nVars { get; set; }
+    public int nvars { get; set; }
     public int comb { get; set; }
-    public Array Vars { get; set; }
+    public Array tableValues { get; set; }
     public bool verif { get; set; }
     public string op { get; set; }
-    public DataGridView Table { get; set; }
+    public DataGridView table { get; set; }
     public Color c { get; set; }
 
-    public TruthTable(int nVars, string op)
+    public TruthTable(int nvars, string op)
     {
-      this.nVars = nVars;
-      comb = Convert.ToInt32(Math.Pow(2, nVars));
-
-      op = op.ToLower();
-      op = op.Replace(" ", ""); op = op.Replace("^", "&&"); op = op.Replace("v", "||"); op = op.Replace("~", "!");
-
+      this.nvars = nvars;
+      comb = Convert.ToInt32(Math.Pow(2, nvars));
+      //op = op.Replace(" ", ""); op = op.Replace("^", "&&"); op = op.Replace("v", "||"); op = op.Replace("~", "!");
       this.op = op;
-
-      Vars = Array.CreateInstance(typeof(bool), comb, 5);
+      tableValues = Array.CreateInstance(typeof(bool), comb, 5);
     }
 
-    public bool Operations(bool a, bool b, bool c, bool d)
+    public bool Operations(bool x, bool y, bool z, bool w)
     {
       bool temp = false;
       try
       {
-        if (nVars == 2) temp = Eval.Execute<bool>(op, new { a, b });
-        if (nVars == 3) temp = Eval.Execute<bool>(op, new { a, b, c });
-        if (nVars == 4) temp = Eval.Execute<bool>(op, new { a, b, c, d });
+        if (nvars == 2) temp = Eval.Execute<bool>(op, new { x, y });
+        if (nvars == 3) temp = Eval.Execute<bool>(op, new { x, y, z });
+        if (nvars == 4) temp = Eval.Execute<bool>(op, new { x, y, z, w });
       }
       catch (Exception ex)
       {
@@ -50,22 +46,22 @@ namespace karnaugh_map_solver
     {
       int temp;
       int div = 1;
-      bool value = true, a, b, c, d, F;
+      bool value = true, x, y, z, w, F;
 
-      for (int indexV = 0; indexV < nVars; indexV++)
+      for (int indexV = 0; indexV < nvars; indexV++)
       {
         temp = comb / (2 * div);
         for (int indexXYZW = 0; indexXYZW < comb; indexXYZW++)
         {
           if (indexXYZW % temp == 0)
             value = !value;
-          Vars.SetValue(value, indexXYZW, indexV);
-          a = Convert.ToBoolean(Vars.GetValue(indexXYZW, 0));
-          b = Convert.ToBoolean(Vars.GetValue(indexXYZW, 1));
-          c = Convert.ToBoolean(Vars.GetValue(indexXYZW, 2));
-          d = Convert.ToBoolean(Vars.GetValue(indexXYZW, 3));
-          F = Operations(a, b, c, d);
-          Vars.SetValue(F, indexXYZW, nVars);
+          tableValues.SetValue(value, indexXYZW, indexV);
+          x = Convert.ToBoolean(tableValues.GetValue(indexXYZW, 0));
+          y = Convert.ToBoolean(tableValues.GetValue(indexXYZW, 1));
+          z = Convert.ToBoolean(tableValues.GetValue(indexXYZW, 2));
+          w = Convert.ToBoolean(tableValues.GetValue(indexXYZW, 3));
+          F = Operations(x, y, z, w);
+          tableValues.SetValue(F, indexXYZW, nvars);
         }
         div *= 2;
       }
@@ -74,21 +70,13 @@ namespace karnaugh_map_solver
     public void CreateTable()
     {
       for (int i = 0; i < comb; i++)
-        for (int j = 0; j < nVars + 1; j++)
-          Table[j, i].Value = ((Convert.ToBoolean(Vars.GetValue(i, j)) == true) ? 1 : 0);
-    }
-
-    public void SelectMinTerms()
-    {
+        for (int j = 0; j < nvars + 1; j++)
+          table[j, i].Value = ((Convert.ToBoolean(tableValues.GetValue(i, j)) == true) ? 1 : 0);
       for (int i = 0; i < comb; i++)
-        if ((Convert.ToBoolean(Vars.GetValue(i, nVars)) == true))
-          Table[nVars, i].Style.BackColor = c;
-    }
-
-    public void ClearMinTerms()
-    {
+        table[nvars, i].Style.BackColor = Color.FromArgb(255, 255, 255);
       for (int i = 0; i < comb; i++)
-        Table[nVars, i].Style.BackColor = Color.FromArgb(61, 61, 61);
+        if ((Convert.ToBoolean(tableValues.GetValue(i, nvars)) == true))
+          table[nvars, i].Style.BackColor = c;
     }
   }
 }
